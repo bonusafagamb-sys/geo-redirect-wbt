3987a557975e82504f8ad07c0d0482c5688e7331
bonusafagamb-sys <bonusafagamb@gmail.com>
2026-04-19 01:04:09 +0100

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
   const links = {
     'KZ': 'https://offertowin.com/l/694c45e6c7ac5658a70cd6a2?sub_id=kukish&sub_id_2=kukish&sub_id_3={sub_id_3}&sub_id_4={sub_id_4}&sub_id_5={sub_id_5}',
     'RU': 'https://offertowin.com/l/694c45e6c7ac5658a70cd6a2?sub_id=kukish&sub_id_2=kukish&sub_id_3={sub_id_3}&sub_id_4={sub_id_4}&sub_id_5={sub_id_5}',
-    'GB': 'https://leadshub.trk-links.com/click?pid=13370&offer_id=6235'
+    'GB': 'https://topplay.site/'
   };
   
   // Ссылка по умолчанию
